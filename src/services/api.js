6d0402bd111e9846e74758ee7ddb88dde80d8fb7
Radwan <www.radwaniq@gmail.com>
2025-09-25 23:25:08 +0300

    Fix environment variables and add debugging
    - Updated .env file to use correct ngrok URL
    - Added environment variable debugging in api.js
    - This will show if environment variables are being loaded properly
    - Fixes the undefinedundefined issue in API URLs

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+console.log("Environment variables:", {
+  REACT_APP_API_URL: process.env.REACT_APP_API_URL,
+  REACT_APP_API_PATH: process.env.REACT_APP_API_PATH,
+  REACT_APP_WS_PATH: process.env.REACT_APP_WS_PATH
+});
+
 const api = axios.create({
   baseURL: `${process.env.REACT_APP_API_URL}${process.env.REACT_APP_API_PATH}`,
 });
